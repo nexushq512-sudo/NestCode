2c3ad1ffc3254cb6de27100bf547c85e0f20f626
Nexus <nexushq512@gmail.com>
2026-08-01 18:15:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { HashRouter } from "react-router-dom"; 
 import Files from "./Files";
-
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Files />
+    <HashRouter>
+      <Files />
+    </HashRouter>
   </StrictMode>
 );
