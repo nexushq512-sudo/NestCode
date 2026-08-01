8d30cd8e17afa96bea9a158600d85e475ad81b98
Nexus <nexushq512@gmail.com>
2026-08-01 18:04:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import Files from "./Files";
-import Popup from "./Popup";
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <>
+  <StrictMode>
     <Files />
-  </>
+  </StrictMode>
 );
