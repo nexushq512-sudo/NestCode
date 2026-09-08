9320173e2483b27df91b341884560bde34cfc86a
Nexus <nexushq512@gmail.com>
2026-09-08 12:51:28 +0000

    Add Android app

---
+++ android/app/src/main/java/com/nexushq/nestcode/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nexushq.nestcode;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nexushq.nestcode',
+  appName: 'NestCode',
+  webDir: 'build'
+};
+
+export default config;
